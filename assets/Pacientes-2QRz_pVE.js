9af8dfb53e5326cd8362de5713db56a425e7a972
Rodrigo Teixeira <rodrigoteixeira.oli0@gmail.com>
2025-06-14 08:00:38 -0300

    commit test

---
+++ assets/Pacientes-2QRz_pVE.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";const c={};function t(n,r){return" pacientes "}const _=e(c,[["render",t]]);export{_ as default};
---
+++ assets/Viagens-CA74_0uo.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";const n={};function r(c,s){return" viagens "}const _=e(n,[["render",r]]);export{_ as default};
